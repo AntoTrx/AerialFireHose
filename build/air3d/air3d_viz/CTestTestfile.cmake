ecdca81090bf6f6cd83e62685597d3270d4535fb
Vincent Chendrawan <71683049+vincentch99@users.noreply.github.com>
2023-03-03 11:29:59 -0800

    Add files via upload

---
+++ build/air3d/air3d_viz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vincent/catkin_ws2/src/air3d/air3d_viz
+# Build directory: /home/vincent/catkin_ws2/build/air3d/air3d_viz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
